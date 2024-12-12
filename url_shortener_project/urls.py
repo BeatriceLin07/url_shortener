774b28ca81d2c130fb8dc266de69e0a85323884e
BeatriceLin07 <beatricelin07@gmail.com>
2024-12-12 20:13:11 +0800

    add view for root url

--- url_shortener_project/urls.py
+++ url_shortener_project/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shorten/', include('shortener.urls')),
+    path('', include('shortener.urls')),
 ]
 
